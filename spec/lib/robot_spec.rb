b473334c26721720318a951f1bb999e2faab4d19
Julian Chan <julian.chan.jk@gmail.com>
2017-05-14 13:43:28 +1000

    rewrote tests for Robot.move to use generators for the x and y values instead

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -1,28 +1,45 @@
 require 'robot'
+require 'rantly/rspec_extensions'
 
 describe Robot do
   describe "#move" do
     context "given a NORTH-facing robot" do
       it "returns a robot one unit up" do
-        expect(Robot.new(0, 0, Directions::NORTH).move).to have_attributes(x: 0, y: 1, direction: Directions::NORTH)
+        property_of {
+          array(2) { range(0, 100) }
+        }.check { |x, y|
+          expect(Robot.new(x, y, Directions::NORTH).move).to have_attributes(x: x, y: y + 1, direction: Directions::NORTH)
+        }
       end
     end
 
     context "given a EAST-facing robot" do
       it "returns a robot one unit right" do
-        expect(Robot.new(1, 1, Directions::EAST).move).to have_attributes(x: 2, y: 1, direction: Directions::EAST)
+        property_of {
+          array(2) { range(0, 100) }
+        }.check { |x, y|
+          expect(Robot.new(x, y, Directions::EAST).move).to have_attributes(x: x + 1, y: y, direction: Directions::EAST)
+        }
       end
     end
 
     context "given a SOUTH-facing robot" do
       it "returns a robot one unit down" do
-        expect(Robot.new(2, 2, Directions::SOUTH).move).to have_attributes(x: 2, y: 1, direction: Directions::SOUTH)
+        property_of {
+          array(2) { range(0, 100) }
+        }.check { |x, y|
+          expect(Robot.new(x, y, Directions::SOUTH).move).to have_attributes(x: x, y: y - 1, direction: Directions::SOUTH)
+        }
       end
     end
 
     context "given a WEST-facing robot" do
       it "returns a robot one unit left" do
-        expect(Robot.new(3, 3, Directions::WEST).move).to have_attributes(x: 2, y: 3, direction: Directions::WEST)
+        property_of {
+          array(2) { range(0, 100) }
+        }.check { |x, y|
+          expect(Robot.new(x, y, Directions::WEST).move).to have_attributes(x: x - 1, y: y, direction: Directions::WEST)
+        }
       end
     end
   end
