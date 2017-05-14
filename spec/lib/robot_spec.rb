7083ac8841126272322b080dacf1684a0d3981fa
Julian Chan <julian.chan.jk@gmail.com>
2017-05-14 17:05:13 +1000

    in robot_spec.rb, assigned the block for generating the coordinates to a variable, and replaced all occurrences of that block with the variable

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -2,12 +2,12 @@ require 'robot'
 require 'rantly/rspec_extensions'
 
 describe Robot do
+  coordinates = proc { array(2) { range(0, 100) } }
+
   describe "#move" do
     context "given a NORTH-facing robot" do
       it "returns a robot one unit up" do
-        property_of {
-          array(2) { range(0, 100) }
-        }.check { |x, y|
+        property_of(&coordinates).check { |x, y|
           expect(Robot.new(x, y, Directions::NORTH).move).to have_attributes(x: x, y: y + 1, direction: Directions::NORTH)
         }
       end
@@ -15,9 +15,7 @@ describe Robot do
 
     context "given a EAST-facing robot" do
       it "returns a robot one unit right" do
-        property_of {
-          array(2) { range(0, 100) }
-        }.check { |x, y|
+        property_of(&coordinates).check { |x, y|
           expect(Robot.new(x, y, Directions::EAST).move).to have_attributes(x: x + 1, y: y, direction: Directions::EAST)
         }
       end
@@ -25,9 +23,7 @@ describe Robot do
 
     context "given a SOUTH-facing robot" do
       it "returns a robot one unit down" do
-        property_of {
-          array(2) { range(0, 100) }
-        }.check { |x, y|
+        property_of(&coordinates).check { |x, y|
           expect(Robot.new(x, y, Directions::SOUTH).move).to have_attributes(x: x, y: y - 1, direction: Directions::SOUTH)
         }
       end
@@ -35,9 +31,7 @@ describe Robot do
 
     context "given a WEST-facing robot" do
       it "returns a robot one unit left" do
-        property_of {
-          array(2) { range(0, 100) }
-        }.check { |x, y|
+        property_of(&coordinates).check { |x, y|
           expect(Robot.new(x, y, Directions::WEST).move).to have_attributes(x: x - 1, y: y, direction: Directions::WEST)
         }
       end
