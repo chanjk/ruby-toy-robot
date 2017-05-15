01dc9a620b484be5de1b0acb58ece934ca033ea7
Julian Chan <julian.chan.jk@gmail.com>
2017-05-15 16:19:13 +1000

    added Simulator module with method play as well as relevant tests

---
+++ lib/simulator.rb
@@ -0,0 +1,47 @@
+require_relative 'directions'
+require_relative 'robot'
+require_relative 'table'
+
+module Simulator
+  def self.play(run_count = nil, robot = nil)
+    if run_count
+      return if run_count <= 0
+      run_count -= 1
+    end
+
+    stringArray = gets.chomp.strip.upcase.split
+    command, args = stringArray[0], stringArray[1..-1].flat_map { |s| s.split(",") }
+
+    next_robot =
+      if command == "PLACE" && args.length >= 3
+        x, y = [args[0], args[1]].map { |s| if s.match(/^\d+$/) then s.to_i end }
+        direction = DIRECTIONS[args[2]]
+
+        Robot.new(x, y, direction) if [x, y, direction].none?(&:nil?)
+      elsif !robot.nil?
+        case command
+          when "MOVE" then robot.move
+          when "LEFT" then robot.turn_left
+          when "RIGHT" then robot.turn_right
+          when "REPORT"
+            puts robot
+            robot
+        end
+      end
+
+    if next_robot && TABLE.contains?(next_robot.x, next_robot.y)
+      play(run_count, next_robot)
+    else
+      play(run_count, robot)
+    end
+  end
+
+  DIRECTIONS = {
+    "NORTH" => Directions::NORTH,
+    "EAST" => Directions::EAST,
+    "SOUTH" => Directions::SOUTH,
+    "WEST" => Directions::WEST,
+  }
+
+  TABLE = Table.new(5, 5)
+end
---
+++ spec/lib/simulator_spec.rb
@@ -0,0 +1,39 @@
+require 'simulator'
+
+describe Simulator do
+  describe "#play" do
+    def play_with_inputs(inputs)
+      allow(Simulator).to receive(:gets).and_return(*inputs)
+      Simulator.play(inputs.length)
+    end
+
+    test_inputs_with_expected = [
+      [["PLACE 0,0,NORTH", "REPORT"], "0,0,NORTH\n"],
+      [["MOVE", "REPORT"], ""],
+      [["LEFT", "REPORT"], ""],
+      [["RIGHT", "REPORT"], ""],
+      [["REPORT"], ""],
+      [["PLACE 1,2,SOUTH", "MOVE", "REPORT"], "1,1,SOUTH\n"],
+      [["PLACE 0,0,NORTH", "MOVE", "REPORT"], "0,1,NORTH\n"],
+      [["PLACE 0,0,NORTH", "LEFT", "REPORT"], "0,0,WEST\n"],
+      [["PLACE 0,0,SOUTH", "MOVE", "RIGHT", "MOVE", "REPORT"], "0,0,WEST\n"],
+      [["PLACE 4,4,EAST", "MOVE", "LEFT", "MOVE", "REPORT"], "4,4,NORTH\n"],
+      [["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT"], "3,3,NORTH\n"],
+      [["PLACE 4,2,WEST", "MOVE", "RIGHT", "MOVE", "MOVE", "REPORT"], "3,4,NORTH\n"],
+      [["PLACE 1,2,NORTH", "MOVE"], ""],
+      [['$@%*^#{*&^*}'], ""],
+      [["PLC12NORTH", "REPORT"], ""],
+      [["PLACE 4,4,SOUTH", '@#{$%^&*"}', "REPORT"], "4,4,SOUTH\n"]
+    ]
+
+    test_inputs_with_expected.each do |inputs, expected|
+      context "#{inputs.join(" | ")}" do
+        it "outputs '#{expected.chomp}'" do
+          expect {
+            play_with_inputs(inputs)
+          }.to output(expected).to_stdout
+        end
+      end
+    end
+  end
+end
